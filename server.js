3aaa9052c86f7611db5c10a6ecc3ffd5aa15e1cd
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-11-18 20:26:09 +1000

    added static dist folder to express

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
-
+app.use(express.static(`${__dirname}/dist`));
 app.get('/', (req, res) => {
   res.sendfile('./dist/index.html');
 });
