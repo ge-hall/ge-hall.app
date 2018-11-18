8ba0a80f15cb05a7d69c8d854ef388e61d018ed9
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-11-18 19:53:50 +1000

    updated server to index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+  res.sendfile('./dist/index.html');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
